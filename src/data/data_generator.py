c34a00efb174b81a10a1d5c4e21be0c36c7ce1dc
Trevor <tkrollins@ucla.com>
2020-02-20 12:01:43 -0800

    data generator can now load squad data directly

--- src/data/data_generator.py
+++ src/data/data_generator.py
@@ -5,7 +5,8 @@ import os
 import re
 import random
 import tensorflow as tf
-import time
+import spacy
+import bert
 
 
 class Node(object):
@@ -26,7 +27,7 @@ class Node(object):
 
 class DataGenerator(object):
 
-    def __init__(self, fname=None, batch_size=1200, neg_rate=1., pos_aug=0, noise=0., shuffle=True):
+    def __init__(self, fname=None, sqaud=False, batch_size=1200, neg_rate=1., pos_aug=0, noise=0., shuffle=True):
         assert int(pos_aug) >= 0
         assert noise >= 0 and noise < 1
         assert neg_rate > 0 and neg_rate <= 1
@@ -44,13 +45,107 @@ class DataGenerator(object):
         random.seed()
 
         if fname is not None:
-            self.read_json(fname)
+            if sqaud is False:
+                self.read_json(fname)
+            else:
+                self.load_squad(fname)
 
     def __len__(self):
         return int(np.ceil(self.data_size / self.batch_size))
 
     def __getitem__(self, idx):
         return self._get_batch()
+
+    def read_json(self, fname):
+        def dict_to_node(child_dict, parent_node=None):
+            child_node = Node(ID=child_dict["id"], data=child_dict["data"], data_type=child_dict["type"],
+                              parent=parent_node, is_impossible=child_dict["is_impossible"])
+            for child in child_dict["children"]:
+                child_node.children.append(
+                    dict_to_node(child, parent_node=child_node))
+            return child_node
+
+        self.children = []
+
+        with open(fname, 'r') as json_file:
+            json_dict = json.loads(json_file.read())
+
+        children = json_dict["root"]
+        for child in children:
+            self.children.append(dict_to_node(child))
+        self.data_size = self._get_data_size()
+        self._init_iter()
+
+    def write_json(self, fname):
+        def node_to_dict(node):
+            node_dict = {"id": node.id, "data": node.data, "type": node.type,
+                         "is_impossible": node.is_impossible, "children": []}
+            for child in node.children:
+                node_dict["children"].append(node_to_dict(child))
+            return node_dict
+
+        data_tree_dict = {"root": []}
+        for child in self.children:
+            assert child.type == "article"
+            data_tree_dict["root"].append(node_to_dict(child))
+
+        with open(fname, 'w') as json_file:
+            json.dump(data_tree_dict, json_file)
+
+    def load_squad(self, fname):
+        with open(fname) as data:
+            squad = json.load(data)["data"]
+
+        nlp = spacy.load("en_core_web_md")
+
+        for a_id, article in enumerate(squad):
+            print(f"A - {a_id}")
+            article_node = Node(ID=a_id, data_type="article")
+            self.children.append(article_node)
+
+            for p_id, paragraph in enumerate(article['paragraphs']):
+                clean_paragraph = bert.albert_tokenization.preprocess_text(
+                    paragraph["context"])
+                nlp_text = nlp(clean_paragraph)
+                paragraph_data = [sent.text for sent in nlp_text.sents]
+                paragraph_node = Node(
+                    data=paragraph_data, parent=article_node, ID=p_id, data_type="paragraph")
+                article_node.children.append(paragraph_node)
+
+                for question in paragraph["qas"]:
+                    question_node = Node(data=question["question"], parent=paragraph_node,
+                                         data_type="question", is_impossible=question["is_impossible"])
+                    paragraph_node.children.append(question_node)
+
+        self.data_size = self._get_data_size()
+        self._init_iter()
+
+    def train_test_split(self, test_size=0.25):
+        assert test_size > 0 and test_size < 1
+        random.shuffle(self.children)
+        train = DataGenerator(batch_size=self.batch_size, pos_aug=self.aug_rate,
+                              noise=self.data_noise, shuffle=self.shuffle, neg_rate=self.neg_sample_rate)
+        test = DataGenerator(batch_size=self.batch_size, pos_aug=self.aug_rate,
+                             noise=self.data_noise, shuffle=self.shuffle, neg_rate=self.neg_sample_rate)
+        test_len = int(test_size * len(self.children))
+        train.children = self.children[test_len:]
+        test.children = self.children[:test_len]
+        train._init_iter()
+        test._init_iter()
+        return train, test
+
+    def transform_data(self, transform_func):
+        def transform_child_data(node, trans_func=transform_func):
+            if node.data is not None:
+                node.data = trans_func(node.data)
+                if node.type == "question":
+                    node.data = node.data[0]
+            for child in node.children:
+                transform_child_data(child)
+
+        for i, child in enumerate(self.children):
+            print(f'ARTICLE {i}')
+            transform_child_data(child)
 
     def _init_iter(self):
         self.active_article_idx = -1
@@ -91,6 +186,12 @@ class DataGenerator(object):
         random.shuffle(par_list)
         return par_list
 
+    def _set_paragraph_list(self):
+        if self.end_epoch is True:
+            return
+        par_list = self.active_article.children.copy()
+        self.paragraph_list = self._create_paragraph_list(par_list)
+
     def _set_next_active_article(self):
         if self.active_article == self.children[-1]:
             self.end_epoch = True
@@ -118,79 +219,10 @@ class DataGenerator(object):
             self.active_question_idx = -1
             self.active_question = None
             self._set_next_active_question()
-            # self._set_paragraph_list()
         else:
             self.active_question_idx += 1
             self.active_question = self.active_paragraph[self.active_question_idx]
             self._set_paragraph_list()
-
-    def _set_paragraph_list(self):
-        if self.end_epoch is True:
-            return
-        par_list = self.active_article.children.copy()
-        self.paragraph_list = self._create_paragraph_list(par_list)
-
-    def train_test_split(self, test_size=0.25):
-        assert test_size > 0 and test_size < 1
-        random.shuffle(self.children)
-        train = DataGenerator(batch_size=self.batch_size, pos_aug=self.aug_rate,
-                         noise=self.data_noise, shuffle=self.shuffle, neg_rate=self.neg_sample_rate)
-        test = DataGenerator(batch_size=self.batch_size, pos_aug=self.aug_rate,
-                        noise=self.data_noise, shuffle=self.shuffle, neg_rate=self.neg_sample_rate)
-        test_len = int(test_size * len(self.children))
-        train.children = self.children[test_len:]
-        test.children = self.children[:test_len]
-        train._init_iter()
-        test._init_iter()
-        return train, test
-
-    def read_json(self, fname):
-        def dict_to_node(child_dict, parent_node=None):
-            child_node = Node(ID=child_dict["id"], data=child_dict["data"], data_type=child_dict["type"],
-                              parent=parent_node, is_impossible=child_dict["is_impossible"])
-            for child in child_dict["children"]:
-                child_node.children.append(
-                    dict_to_node(child, parent_node=child_node))
-            return child_node
-
-        self.children = []
-
-        with open(fname, 'r') as json_file:
-            json_dict = json.loads(json_file.read())
-
-        children = json_dict["root"]
-        for child in children:
-            self.children.append(dict_to_node(child))
-        self.data_size = self._get_data_size()
-        self._init_iter()
-
-    def write_json(self, fname):
-        def node_to_dict(node):
-            node_dict = {"id": node.id, "data": node.data, "type": node.type,
-                         "is_impossible": node.is_impossible, "children": []}
-            for child in node.children:
-                node_dict["children"].append(node_to_dict(child))
-            return node_dict
-
-        data_tree_dict = {"root": []}
-        for child in self.children:
-            assert child.type == "article"
-            data_tree_dict["root"].append(node_to_dict(child))
-
-        with open(fname, 'w') as json_file:
-            json.dump(data_tree_dict, json_file)
-
-    def transform_data(self, transform_func):
-        def transform_child_data(node, trans_func=transform_func):
-            if node.data is not None:
-                node.data = trans_func(node.data)
-                if node.type == "question":
-                    node.data = node.data[0]
-            for child in node.children:
-                transform_child_data(child)
-
-        for child in self.children:
-            transform_child_data(child)
 
     def _get_data_size(self):
         size = 0
@@ -216,37 +248,34 @@ class DataGenerator(object):
         return count
 
     def _get_batch(self):
-        X_batch = []
+        X_question_batch = []
+        X_paragraph_batch = []
         y_batch = []
         self.count += 1
         for _ in range(self.batch_size):
-            X, y = self._get_next_datapoint()
-            X_batch.append(X)
+            X_question, X_paragraph, y = self._get_next_datapoint()
+            X_question_batch.append(X_question)
+            X_paragraph_batch.append(X_paragraph)
             y_batch.append(y)
             self._update_iter()
             if self.end_epoch is True:
-                print(f"ACTUAL BATCH COUNT: {self.count}")
                 self._on_epoch_end()
                 break
-        return np.array(X_batch), np.array(y_batch)
+        return np.array([X_question_batch, X_paragraph_batch]), np.array(y_batch)
 
-    
     def _on_epoch_end(self):
         random.shuffle(self.children)
         for article in self.children:
             random.shuffle(article.children)
         self._init_iter()
 
-
     def _get_next_datapoint(self):
         current_par = self.paragraph_list[self.PAR_LIST_IDX]
-        # X = np.array([np.array(self.active_question.data),
-        #               np.array(current_par.data)])
-        X = np.array([self.active_question.data,
-                      current_par.data])
+        X_question = np.array(np.array(self.active_question.data))
+        X_paragraph = np.array(current_par.data)
         y = int((self.active_paragraph.id == current_par.id)
                 and (self.active_question.is_impossible is False))
-        return X, y
+        return (X_question, X_paragraph, y)
 
     def _update_iter(self):
         self.PAR_LIST_IDX += 1
@@ -254,35 +283,3 @@ class DataGenerator(object):
             self.PAR_LIST_IDX = 0
             self._set_next_active_question()
 
-
-# class DataGenerator(tf.keras.utils.Sequence):
-
-
-# def atoi(text):
-#     return int(text) if text.isdigit() else text
-
-
-# def natural_keys(text):
-#     return [atoi(c) for c in re.split(r'(\d+)', text)]
-
-# if __name__ == "__main__":
-#     article_data = np.load(
-#         "/Users/tkrollins/OneDrive/Courses/capstone/question-answering/data/interim/article_data.npy", allow_pickle=True)
-#     question_data = pd.read_csv(
-#         "/Users/tkrollins/OneDrive/Courses/capstone/question-answering/data/interim/questions.csv")
-
-    # data_tree = DataTree()
-
-    # for i, article in enumerate(article_data):
-    #     article_node = Node(ID=i, data_type="article")
-    #     for j, paragraph in enumerate(article):
-    #         paragraph_node = Node(
-    #             data=paragraph["sents"], parent=article_node, ID=j, data_type="paragraph")
-    #         questions = question_data.loc[(question_data["article"] == i) & (
-    #             question_data["paragraph"] == j)]
-    #         for _, q in questions.iterrows():
-    #             question_node = Node(data=q["question_text"], parent=paragraph_node,
-    #                                  data_type="question", is_impossible=q["is_impossible"])
-    #             paragraph_node.add_child_node(question_node)
-    #         article_node.add_child_node(paragraph_node)
-    #     data_tree.children.append(article_node)
